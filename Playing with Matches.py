20e5672d62f2a8f1ec56ee5400f59d2b047a7580
ADITHYA G <84570189+Adithyagaddam@users.noreply.github.com>
2022-09-05 15:37:02 +0530

    Create Playing with Matches.py

---
+++ Playing with Matches.py
@@ -0,0 +1,22 @@
+n=int(input())
+for i in range(n):
+    a,b=map(int,input().split())
+    s=a+b
+    c=str(s)
+    count=0
+    for i in range(len(c)):
+        if c[i]=='1':
+            count=count+2
+        elif c[i]=='7':
+            count=count+3
+        elif c[i]=='4':
+            count+=4
+        elif c[i]=='2' or c[i]=='3' or c[i]=='5':
+            count+=5
+        elif c[i]=='0' or c[i]=='6' or c[i]=='9':
+            count+=6
+        else:
+            count+=7
+    print(count)
+    
+    
