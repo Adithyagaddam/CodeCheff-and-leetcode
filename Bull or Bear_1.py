f7a1d0fd44103691ae4187033fc26d947cbba3d1
ADITHYA G <84570189+Adithyagaddam@users.noreply.github.com>
2022-12-13 22:19:46 +0530

    Create Bull or Bear_1.py

---
+++ Bull or Bear_1.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for i in range(int(input())):
+    a,b=map(int,input().strip().split())
+    if a>b:
+        print("LOSS")
+    elif a==b:
+        print("NEUTRAL")
+    else:
+        print("PROFIT")
