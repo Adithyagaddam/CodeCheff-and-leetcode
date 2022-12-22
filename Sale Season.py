d9e88eccd07d9310fc50e19c003021cd0c68c8d0
ADITHYA G <84570189+Adithyagaddam@users.noreply.github.com>
2022-12-22 21:53:14 +0530

    Create Sale Season.py

---
+++ Sale Season.py
@@ -0,0 +1,10 @@
+for i in range(int(input())):
+    a=int(input())
+    if a<=100:
+        print(a)
+    elif a>100 and a<=1000:
+        print(a-25)
+    elif a>1000 and a<=5000:
+        print(a-100)
+    else:print(a-500)
+        
