30462c1c804c14be41e2f0088209662923f7b409
ADITHYA G <84570189+Adithyagaddam@users.noreply.github.com>
2023-03-04 22:31:20 +0530

    Create Better Deal.py

---
+++ Better Deal.py
@@ -0,0 +1,12 @@
+# cook your dish here
+t = int(input())
+
+for i in range(t):
+    x, y = input().split()
+    x, y = int(x), int(y)
+    if 100-100*x/100 == 200-200*y/100:
+        print("BOTH")
+    elif 100-100*x/100 < 200-200*y/100:
+        print("FIRST")
+    else:
+        print("SECOND")
