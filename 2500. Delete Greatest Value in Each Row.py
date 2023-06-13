cba499c254e101e15551d2eac57faeb9ee1a2cec
ADITHYA G <84570189+Adithyagaddam@users.noreply.github.com>
2023-06-13 21:15:14 +0530

    Create 2500. Delete Greatest Value in Each Row.py

---
+++ 2500. Delete Greatest Value in Each Row.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def deleteGreatestValue(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        ans = 0
+        for i in range(len(grid[0])):
+            m1 = -1
+            for j in range(len(grid)):
+                mx = max(grid[j])
+                m1 = max(m1,mx)
+                grid[j].remove(mx)
+            ans += m1
+        return ans
